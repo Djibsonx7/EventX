c31527aff89664d49c4423bd0ce5b686618c842a
Djibsonx7 <59049134+Djibsonx7@users.noreply.github.com>
2026-04-20 21:43:32 +0000

    feat: add join page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import HomePage from './pages/HomePage'
 import CreateSessionPage from './pages/CreateSessionPage'
+import JoinSessionPage from './pages/JoinSessionPage'
 
 export default function App() {
   const [page, setPage] = useState('home')
@@ -9,10 +10,14 @@ export default function App() {
     return <CreateSessionPage />
   }
 
+  if (page === 'join') {
+    return <JoinSessionPage onBackClick={() => setPage('home')} />
+  }
+
   return (
     <HomePage
       onCreateClick={() => setPage('create')}
-      onJoinClick={() => alert('Join flow coming next')}
+      onJoinClick={() => setPage('join')}
     />
   )
 }
