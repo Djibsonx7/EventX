bb72c8c731e8775a9cd8215c8d1d29e601431e4c
Djibsonx7 <59049134+Djibsonx7@users.noreply.github.com>
2026-04-20 21:09:06 +0000

    feat: add Tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7c3aed',
+        accent: '#ec4899',
+      }
+    },
+  },
+  plugins: [],
+}
