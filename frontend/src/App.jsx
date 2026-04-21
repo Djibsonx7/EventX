ee273dce94b76ae4a70b569f2c3f35738f00caed
Djibsonx7 <59049134+Djibsonx7@users.noreply.github.com>
2026-04-21 00:35:39 +0000

    feat: connect simulate game start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,6 +36,7 @@ export default function App() {
       <PlayerWaitingRoomPage
         mode={sessionMode}
         onBackClick={() => setPage('home')}
+        onStartClick={() => setPage('game')}
       />
     )
   }
